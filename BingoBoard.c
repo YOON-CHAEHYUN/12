e66457ab67a9605a7d1f66438daed1867d7a1eeb
YOON-CHAEHYUN <yunch0202@naver.com>
2022-11-17 17:46:44 +0900

    Update BingoBoard.c

--- BingoBoard.c
+++ BingoBoard.c
@@ -1,21 +1,22 @@
 #include "BingoBoard.h"
 #define bingo_hole -1
 
-int bingoBoard[n_size][n_size];
+static int bingoBoard[n_size][n_size];
+static int numberStatus[n_size*n_size];
 
 void bingo_init(void){
 	int i, j, cnt=1;
 	for(i=0;i<n_size;i++)
 		for(j=0;j<n_size;j++){
-			if(cnt==15){
+			if(cnt==15)
+			{
 				bingoBoard[i][j]=bingo_hole;
+				numberStatus[cnt-1]=bingo_hole;
 				cnt++;
 			}
-			else{
-				
-				bingoBoard[i][j]=cnt;
-				cnt++;
-			}
+			
+			numberStatus[cnt-1]=i*n_size+j;
+			bingoBoard[i][j]=cnt++;
 		}
 }
 void bingo_print(void){
@@ -36,7 +37,11 @@ void bingo_print(void){
 	printf("============================\n");
 }
 void bingo_inputNum(int sel){
+	int i,j;
 	
+	i=numberStatus[sel-1]/n_size;
+	j=numberStatus[sel-1]%n_size;
+	bingoBoard[i][j]=bingo_hole;
 }
 int bingo_countCompletedLine(void){
 	
