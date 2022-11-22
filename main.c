a74b7f20c829d749b14f27e66b8a5cde9320e938
YOON-CHAEHYUN <yunch0202@naver.com>
2022-11-22 17:33:25 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,25 @@
 #include "BingoBoard.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int get_number(void)
+{
+	int selNum=0;
+	
+	do{
+		printf("select a number: ");
+		scanf("%d",&selNum);
+		fflush(stdin);//숫자 하나 얻었으면 나머지 버릴 때 
+		if (selNum<1||selNum>n_size*n_size||bingo_checkNum(selNum)==BINGO_NUMSTATUS_ABSENT)
+			{
+				printf("%i is not on the board! select other one.\n");
+			}
+	}while(selNum<1||selNum>n_size*n_size||bingo_checkNum(selNum)==BINGO_NUMSTATUS_ABSENT);
+	return selNum;
+}
+
 int main(int argc, char *argv[]) {
 	
-	int i,num;
+	int i,num, selNum,imsi;
 	
 	num=n_size*n_size;	
 	
@@ -18,9 +34,14 @@ int main(int argc, char *argv[]) {
 	//SELECT NUMBER
 	//UPDATE THE STATUS
 	
-	bingo_init();
-	bingo_print();
-	
+	imsi=0;
+	while(imsi<n_bingo){
+		bingo_init();
+		bingo_print();
+		selNum=get_number();
+		bingo_inputNum(selNum);
+		imsi=bingo_countCompletedLine();
+	}
 	printf("\n\n\n\n\n\n\n\n\n");
 	printf("@@@@@@@@@@@VICTORY@@@@@@@@@@@@@@@@");
 	return 0;
