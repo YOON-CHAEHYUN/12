7da5e63dfdba644fbe9e2442b18df2e175ee6c1f
YOON-CHAEHYUN <yunch0202@naver.com>
2022-11-22 23:31:40 +0900

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,11 @@ int get_number(void)
 {
 	int selNum=0;
 	
+	
 	do{
 		printf("select a number: ");
 		scanf("%d",&selNum);
-		fflush(stdin);//숫자 하나 얻었으면 나머지 버릴 때 
+		fflush(stdin);//???? ??? ??????? ?????? ???? ?? 
 		if (selNum<1||selNum>n_size*n_size||bingo_checkNum(selNum)==BINGO_NUMSTATUS_ABSENT)
 			{
 				printf("%i is not on the board! select other one.\n");
@@ -23,8 +24,9 @@ int get_number(void)
 int main(int argc, char *argv[]) {
 	
 	int i,num, selNum,imsi;
+	num=n_size*n_size;
 	
-	num=n_size*n_size;	
+	srand((unsigned int)time(NULL));	
 	
 	printf("**********************************\n");
 	printf("             BINGO                \n");
@@ -38,10 +40,13 @@ int main(int argc, char *argv[]) {
 	bingo_init();
 	while(imsi<n_bingo){
 		bingo_print();
+		printf("Bingo: %i\n",imsi);
 		selNum=get_number();
 		bingo_inputNum(selNum);
 		imsi=bingo_countCompletedLine();
+		
 	}
+	bingo_print();
 	printf("\n\n\n\n\n\n\n\n\n");
 	printf("@@@@@@@@@@@VICTORY@@@@@@@@@@@@@@@@");
 	return 0;
